80151b982b937fdb7e6aa31dc45d6d671817a8d9
Hsieh Meng Hua <qprs71@gmail.com>
2016-05-21 23:44:21 +0800

    no message

--- 0/connectDB.php
+++ 0/connectDB.php
@@ -1,15 +1,15 @@
 <?php
-$hostname = "fdb14.biz.nf";
-$username = "2034119_project";
-$password = "R9!6fsX5@3de";
-$DBname = "2034119_project";
+$hostname = "localhost";
+$username = "root";
+$password = "MyNewPass";
+$DBname = "project_groupping";
 
 // Create connection
 $conn = new mysqli($hostname, $username, $password, $DBname);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("<script>alert($conn->connect_error);</script>");
 } 
-echo "Database connected successfully"."<br>";
+echo "<script>alert("connected");</script>";
 ?>
